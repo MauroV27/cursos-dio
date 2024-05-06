5c06dd1d9a8792c64b409cbb15059db6ef509979
MauroV27 <maurovictor13@gmail.com>
2024-05-05 23:21:46 -0300

    Código para o Desafio - Implementando Validações de Testes Unitários com C#.

---
+++ dotnetBackend/DesafioTestesUnitarios/TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
@@ -0,0 +1,64 @@
+using TestesUnitarios.Desafio.Console.Services;
+
+namespace TestesUnitarios.Desafio.Tests;
+
+public class ValidacoesStringTests
+{
+    private ValidacoesString _validacoes = new ValidacoesString();
+
+    [Fact]
+    public void DeveRetornar6QuantidadeCaracteresDaPalavraMatrix()
+    {
+        // Arrange
+        var texto = "Matrix";
+        var resultadoEsperado = 6;
+
+        // Act
+        var resultado = _validacoes.RetornarQuantidadeCaracteres(texto);
+
+        // Assert
+        Assert.Equal(resultadoEsperado, resultado);
+    }
+
+    [Fact]
+    public void DeveConterAPalavraQualquerNoTexto()
+    {
+        // Arrange
+        var texto = "Esse é um texto qualquer";
+        var textoProcurado = "qualquer";
+
+        // Act
+        var res = _validacoes.ContemCaractere(texto, textoProcurado);
+
+        // Assert
+        Assert.True(res);
+    }
+
+    [Fact]
+    public void NaoDeveConterAPalavraTesteNoTexto()
+    {
+        // Arrange
+        var texto = "Esse é um texto qualquer";
+        var textoProcurado = "teste";
+
+        // Act
+        var res = _validacoes.ContemCaractere(texto, textoProcurado);
+
+        // Assert
+        Assert.False(res);
+    }
+
+    [Fact]
+    public void TextoDeveTerminarComAPalavraProcurado()
+    {
+        // Arrange
+        var texto = "Começo, meio e fim do texto procurado";
+        var textoProcurado = "procurado";
+
+        // Act
+        var res = _validacoes.TextoTerminaCom(texto, textoProcurado);
+
+        // Assert
+        Assert.True(res);
+    }
+}
