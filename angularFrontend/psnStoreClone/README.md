ea6ab62c54cffe529c20c28f83d576710e795a1f
MauroV27 <maurovictor13@gmail.com>
2024-11-10 12:09:57 -0300

    Código para o desafio - Recriando a Interface da PlayStation Store com Angular

---
+++ angularFrontend/psnStoreClone/README.md
@@ -0,0 +1,10 @@
+# Clone da PlayStation Store 
+
+> Feito para o desafio do bootcamp *Deal - Spring Boot e Angular (17+)* da DIO.
+
+Usando Angular e componentização dos elementos da página web foi desenvolvida uma versão simplificada da página da PSN Store.
+
+**Resultado final**
+![](./.github/resultado%20final%20dio-angular-psn-clone.png)
+
+
---
+++ angularFrontend/psnStoreClone/src/app/components/card/card.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { CardLabelComponent } from './card-label/card-label.component';
+import { CardPricingComponent } from './card-pricing/card-pricing.component';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [
+    CardLabelComponent,
+    CardPricingComponent
+  ],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent implements OnInit {
+
+  @Input()
+	gameCover:string = ""
+	@Input()
+	gameLabel:string=""
+	@Input()
+	gameType:string ="XPTO | PS4"
+	@Input()
+	gamePrice:string = "R$ 399,90"
+
+  constructor() { }
+
+  ngOnInit(): void {}
+
+}
